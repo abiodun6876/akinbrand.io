24dfdde78667825adf2ed01c21c178777936160b
AKINPELU Abiodun Moses <abiodunmosesakinpelu@gmail.com>
2023-09-19 05:33:14 -0700

    moses
    What We Do
    We utilize industry best practices and cutting-edge technologies to ensure the development of robust, scalable, and future-proof digital products.
    
    Previous

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['akinbrand.com.ng','tailwindui.com','.unsplash.comimages','www.w3.org','images.unsplash.com'],
+      
+    },
+  };
+
+  
+  module.exports = nextConfig;
